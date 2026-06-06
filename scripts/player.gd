92d7d9baa1289edb97faffd52682d101e5c6bf3b
passador-dor-dor <henriquepassador09@gmail.com>
2026-06-06 15:13:52 -0300

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+	
+	
+func die():
+	get_tree().reload_current_scene()
+	
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("reset"): 
+		get_tree().reload_current_scene()
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("left", "right")
+# Inverte o sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+# Altera a animação
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("walk")
+	else:
+		animated_sprite_2d.play("jump")
+# Apply movement
+	if direction:	
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+	move_and_slide()
